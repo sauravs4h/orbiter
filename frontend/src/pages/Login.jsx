1e0414838d54a5b971eb036d36dafdc4ece54383
sauravs4h <sauravs4h@gmail.com>
2024-02-25 00:44:22 +0530

    change style

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -10,9 +10,8 @@ import {
 } from "../redux/Auth/action";
 import axios from "axios";
 
-
 //const api="http://localhost:8080";
-const api="https://orbiter.onrender.com"
+const api = "https://orbiter.onrender.com";
 
 function Login() {
   const navigate = useNavigate();
@@ -21,12 +20,8 @@ function Login() {
     return store.authReducer;
   });
 
-  
-
   const handleInputChange = (e) => {
     setData({ ...data, [e.target.name]: e.target.value });
-
-    
   };
 
   let dispatch = useDispatch();
@@ -38,10 +33,7 @@ function Login() {
 
     try {
       let payload = data;
-      let resData = await axios.post(
-        `${api}/user/login`,
-        payload
-      );
+      let resData = await axios.post(`${api}/user/login`, payload);
 
       let result = resData.data;
       let token = result.token;
@@ -57,41 +49,48 @@ function Login() {
   };
 
   return (
-    <div>
-      <Navbar />
-
-      <div className="login-form-container">
-        <h2>Login</h2>
-        <form onSubmit={handleSubmit}>
-          <div className="form-group">
-            <label htmlFor="email">Email</label>
-            <input
-              type="email"
-              id="email"
-              name="email"
-              value={data.email}
-              placeholder="Email"
-              required
-              onChange={handleInputChange}
-            />
-          </div>
-          <div className="form-group">
-            <label htmlFor="password">Password</label>
-            <input
-              type="password"
-              id="password"
-              name="password"
-              value={data.password}
-              placeholder="Password"
-              required
-              onChange={handleInputChange}
-            />
-          </div>
-          <button type="submit">Login</button>
-        </form>
-        <p>
-          Don't have an account? <button onClick={() => navigate("/signup")}>Sign up</button>
-        </p>
+    <div id="loginpage">
+      <div id="leftside">
+        <img
+          src="https://socilious.com/images/growth-analysis.gif"
+          alt="Orbiter"
+        />
+      </div>
+      <div id="rightside">
+        <div className="login-form-container">
+          <h2>Orbiter</h2>
+          <form onSubmit={handleSubmit}>
+            <div className="form-group">
+              <input
+                type="email"
+                id="email"
+                name="email"
+                value={data.email}
+                placeholder="Email"
+                required
+                onChange={handleInputChange}
+              />
+            </div>
+            <div className="form-group">
+              <input
+                type="password"
+                id="password"
+                name="password"
+                value={data.password}
+                placeholder="Password"
+                required
+                onChange={handleInputChange}
+              />
+            </div>
+            <button type="submit">Login</button>
+          </form>
+        </div>
+        <div className="goforsignup">
+          <p>
+            Don't have an account?{" "}
+            <a onClick={() => navigate("/signup")}>Sign up</a>
+          </p>
+        </div>
       </div>
     </div>
   );
--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -5,7 +5,7 @@ import Navbar from "../components/Navbar";
 import axios from "axios";
 
 //const api="http://localhost:8080"
-const api="https://orbiter.onrender.com"
+const api = "https://orbiter.onrender.com";
 
 function Signup() {
   const [signupData, setSignupData] = useState({
@@ -19,108 +19,110 @@ function Signup() {
 
   const handleInput = (e) => {
     setSignupData({ ...signupData, [e.target.name]: e.target.value });
-    
   };
 
   const navigate = useNavigate();
 
   async function handleSubmit(event) {
     event.preventDefault();
-    
-    try {
-      let payload=signupData;
 
-      let resData=await axios.post(`${api}/user/signup`,payload);
-      let result=resData.data
-      alert(result.message)
-      
-      
+    try {
+      let payload = signupData;
+
+      let resData = await axios.post(`${api}/user/signup`, payload);
+      let result = resData.data;
+      alert(result.message);
     } catch (error) {
-      alert(error.message)
+      alert(error.message);
     }
-    navigate("/login"); 
+    navigate("/login");
   }
   return (
-    <div>
-      <Navbar />
-
-      <div className="signup-form-container">
-        <h2>Sign Up</h2>
-        <form onSubmit={handleSubmit}>
-          <div className="form-group">
-            <label htmlFor="firstName">First Name</label>
-            <input
-              type="text"
-              id="firstName"
-              name="firstName"
-              value={signupData.firstName}
-              onChange={handleInput}
-              placeholder="FirstName"
-              required
-            />
-          </div>
-          <div className="form-group">
-            <label htmlFor="lastName">Last Name</label>
-            <input
-              type="text"
-              id="lastName"
-              name="lastName"
-              value={signupData.lastName}
-              onChange={handleInput}
-              placeholder="lastName"
-              required
-            />
-          </div>
-          <div className="form-group">
-            <label htmlFor="userName">Username</label>
-            <input
-              type="text"
-              id="userName"
-              name="userName"
-              value={signupData.userName}
-              onChange={handleInput}
-              placeholder="UserName"
-              required
-            />
-          </div>
-          <div className="form-group">
-            <label htmlFor="email">Email</label>
-            <input
-              type="email"
-              id="email"
-              name="email"
-              value={signupData.email}
-              onChange={handleInput}
-              placeholder="Email"
-              required
-            />
-          </div>
-          <div className="form-group">
-            <label htmlFor="mobile_no">Mobile Number</label>
-            <input
-              type="Number"
-              id="mobile_no"
-              name="mobile_no"
-              value={signupData.mobile_no}
-              onChange={handleInput}
-              placeholder="Mobile Number"
-              required
-            />
-          </div>
-          <div className="form-group">
-            <label htmlFor="password">Password</label>
-            <input
-              type="password"
-              id="password"
-              name="password"
-              value={signupData.password}
-              onChange={handleInput}
-              placeholder="Password"
-              required
-            />
-          </div>
-          <button type="submit">Sign Up</button>
-        </form>
+    <div id="signuppage">
+      <div id="leftside">
+        <img
+          src="https://socilious.com/images/business-team%20(1).gif"
+          alt="Orbiter"
+        />
+      </div>
+      <div id="rightside">
+        <div className="signup-form-container">
+          <h2>Orbiter</h2>
+          <form onSubmit={handleSubmit}>
+            <div className="form-group">
+              <input
+                type="text"
+                id="firstName"
+                name="firstName"
+                value={signupData.firstName}
+                onChange={handleInput}
+                placeholder="First Name"
+                required
+              />
+            </div>
+            <div className="form-group">
+              <input
+                type="text"
+                id="lastName"
+                name="lastName"
+                value={signupData.lastName}
+                onChange={handleInput}
+                placeholder="Last Name"
+                required
+              />
+            </div>
+            <div className="form-group">
+              <input
+                type="text"
+                id="userName"
+                name="userName"
+                value={signupData.userName}
+                onChange={handleInput}
+                placeholder="Username"
+                required
+              />
+            </div>
+            <div className="form-group">
+              <input
+                type="email"
+                id="email"
+                name="email"
+                value={signupData.email}
+                onChange={handleInput}
+                placeholder="Email"
+                required
+              />
+            </div>
+            <div className="form-group">
+              <input
+                type="Number"
+                id="mobile_no"
+                name="mobile_no"
+                value={signupData.mobile_no}
+                onChange={handleInput}
+                placeholder="Mobile Number"
+                required
+              />
+            </div>
+            <div className="form-group">
+              <input
+                type="password"
+                id="password"
+                name="password"
+                value={signupData.password}
+                onChange={handleInput}
+                placeholder="Password"
+                required
+              />
+            </div>
+            <button type="submit">Sign Up</button>
+          </form>
+        </div>
+        <div className="goforlogin">
+          <p>
+            Have an account? <a onClick={() => navigate("/login")}>Login</a>
+          </p>
+        </div>
       </div>
     </div>
   );
