977b5dcaa55271e8080fd187a1c6fb80fc179aef
sauravs4h <sauravs4h@gmail.com>
2024-02-24 13:44:19 +0530

    persist

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -1,9 +1,17 @@
-import {combineReducers, legacy_createStore} from "redux"
+import { combineReducers, legacy_createStore } from "redux";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import { reducer as authReducer } from "./Auth/reducer";
 
-import {reducer as authReducer} from "./Auth/reducer"
+const persistConfig = {
+  key: "root",
+  storage,
+};
 
-const rootReducer=combineReducers({
-    authReducer
-})
+const rootReducer = combineReducers({
+  authReducer,
+});
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
-export const store = legacy_createStore(rootReducer);
+export const store = legacy_createStore(persistedReducer);
+export const persistor = persistStore(store);
