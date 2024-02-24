a0b7a79bad9cd89bd34e2b9530d5e0131fba1e3e
sauravs4h <sauravs4h@gmail.com>
2024-02-25 02:37:13 +0530

    change style

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -1,37 +1,38 @@
 import React from "react";
-import "../css/Profile.css"
+import "../css/Profile.css";
 
 function Profile() {
-  let firstName = "saurav";
+  let firstName = "Saurav";
   let lastName = "Sharma";
   let userName = "sauravs4h";
   let email = "sauravs4h@gmail.com";
-  let mobile_no = 7340165574;
+  let mobile_no = "7340165574";
 
   return (
-    <div>
-      <div className="header">
-        <div className="headerleft">
+    <div className="profile">
+      <div className="profile-header">
+        <div className="profile-header-left">
           <img
-            src="https://instagram.fjai1-4.fna.fbcdn.net/v/t51.2885-19/146842709_412203473393518_7977264176446510686_n.jpg?stp=dst-jpg_s320x320&_nc_ht=instagram.fjai1-4.fna.fbcdn.net&_nc_cat=110&_nc_ohc=iYBIfbmOFe4AX95sLZ2&edm=AOQ1c0wBAAAA&ccb=7-5&oh=00_AfDa1b9htPnCrJ3-ABthb7b_-Qs3fYqAqj-ZxSfxu68C8A&oe=65DED665&_nc_sid=8b3546"
+            src="https://www.mecgale.com/wp-content/uploads/2017/08/dummy-profile.png"
             alt="saurav"
+            className="profile-picture"
           />
         </div>
-        <div className="headerRight">
-          <div className="headerRightline1">
+        <div className="profile-header-right">
+          <div className="profile-header-top">
             <h3>{userName}</h3>
-            <button>Edit Profile</button>
+            <button className="edit-profile-btn">Edit Profile</button>
           </div>
-          <div className="headerRightline2">
-            <h3>175 Post</h3>
-            <h3>224 followers</h3>
-            <h3>782 following</h3>
+          <div className="profile-header-middle">
+            <span className="post-count">175 Posts</span>
+            <span className="followers-count">224 Followers</span>
+            <span className="following-count">782 Following</span>
           </div>
-          <h3>
-            {firstName} {lastName}
-          </h3>
-          <h3>{email}</h3>
-          <h3>{mobile_no}</h3>
+          <div className="profile-header-bottom">
+            <h3>{firstName} {lastName}</h3>
+            <h3>{email}</h3>
+            <h3>{mobile_no}</h3>
+          </div>
         </div>
       </div>
     </div>
