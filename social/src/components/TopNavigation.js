ae8eb62162d7984f5311d2cce4fda9fb93553046
sauravs4h <sauravs4h@gmail.com>
2024-03-06 18:22:44 +0530

    a

---
+++ social/src/components/TopNavigation.js
@@ -0,0 +1,77 @@
+import "./TopNavigation.css";
+
+const TopNavigation = () => {
+  return (
+    <div className="top-nav">
+      <div className="logo-1-1-parent">
+        <img className="logo-1-1" alt="" src="/OrbiterLogo@2x.png" />
+        <div className="frame-parent">
+          <div className="call-192-svgrepocom-parent">
+            <img
+              className="call-192-svgrepocom-icon"
+              alt=""
+              src="/phonelogo@2x.png"
+            />
+            <div className="div">+91 7357752376</div>
+          </div>
+          <div className="call-192-svgrepocom-parent">
+            <img
+              className="call-192-svgrepocom-icon"
+              alt=""
+              src="/mailLogo@2x.png"
+            />
+            <div className="div">Ceo@socilious.com</div>
+          </div>
+          <div className="component-1">
+            <div className="component-1-child" />
+            <div className="component-1-item" />
+            <b className="apply-for-investment1">Apply for Investment</b>
+          </div>
+        </div>
+      </div>
+      <nav className="component-26-parent">
+        <div className="component-26">
+          <div className="home1">Home</div>
+          <div className="component-26-child" />
+        </div>
+        <img className="frame-item" alt="" src="/star@2x.png" />
+        <div className="component-27">
+          <div className="home1">Socivo</div>
+          <div className="component-26-child" />
+        </div>
+        <img className="frame-inner" alt="" src="/star-2.svg" />
+        <div className="component-28">
+          <div className="home1">Networking</div>
+          <img className="component-28-child" alt="" />
+        </div>
+        <img className="frame-inner" alt="" src="/star-2.svg" />
+        <div className="component-29">
+          <div className="home1">Entrechat</div>
+          <img className="component-28-child" alt="" />
+        </div>
+        <img className="frame-inner" alt="" src="/star-2.svg" />
+        <div className="component-30">
+          <div className="home1">Community</div>
+          <img className="component-26-child" alt="" />
+        </div>
+        <img className="frame-inner" alt="" src="/star-2.svg" />
+        <div className="component-33">
+          <div className="home1">News</div>
+          <img className="component-28-child" alt="" />
+        </div>
+        <img className="frame-inner" alt="" src="/star-2.svg" />
+        <div className="component-31">
+          <div className="home1">About Us</div>
+          <img className="component-28-child" alt="" />
+        </div>
+        <img className="frame-inner" alt="" src="/star-2.svg" />
+        <div className="component-32">
+          <div className="home1">Contact Us</div>
+          <img className="component-28-child" alt="" />
+        </div>
+      </nav>
+    </div>
+  );
+};
+
+export default TopNavigation;
---
+++ social/src/pages/HeroSection.js
@@ -0,0 +1,44 @@
+import TopNavigation from "../components/TopNavigation";
+// import "./HeroSection.css";
+
+const HeroSection = () => {
+  return (
+    <div className="hero-section">
+      {/* <div className="welcome-to-orbiter">Welcome to Orbiter</div>
+      <div className="connect-with-parent">
+        <div className="connect-with">Connect with</div>
+        <div className="component-5">
+          <div className="mentors">Mentors</div>
+          <div className="investors">Investors</div>
+          <div className="entreprenuers">Entreprenuers</div>
+          <div className="startups">Startups</div>
+          <div className="incubators">Incubators</div>
+          <div className="b-school">B-School</div>
+          <div className="companies">Companies</div>
+          <div className="organization">Organization</div>
+          <div className="virtual-team">Virtual Team</div>
+        </div>
+      </div>
+      <div className="funded-by-rajasthan-goverment-parent">
+        <div className="funded-by-rajasthan-container">
+          <span>{`Funded by `}</span>
+          <b>Rajasthan Goverment</b>
+        </div>
+        <img className="frame-child" alt="" src="/line-23.svg" />
+        <div className="funded-by-rajasthan-container">
+          <span>{`Incubated in `}</span>
+          <b>IIT, Mandi</b>
+        </div>
+      </div>
+      <img className="hero-image-icon" alt="" src="/hero-image.svg" />
+      <img
+        className="calender-dynamic-color-icon"
+        alt=""
+        src="/calenderdynamiccolor@2x.png"
+      /> */}
+      <TopNavigation />
+    </div>
+  );
+};
+
+export default HeroSection;
